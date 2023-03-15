6ce66fab457f467e18687317d3d3a431801a2d6e
gwx-nakao <gwx.nakao@outlook.jp>
2023-03-15 13:23:18 +0900

    update

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,4 @@
 function doPrint(){
     var inputText = document.getElementById('inputText');
     console.log(inputText.value);
-    var msgArea = document.getElementById('msgArea');
-    msgArea.innerText = inputText.value;
 }
